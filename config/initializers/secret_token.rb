b64c237398a5753babe13a7b2aaea9c93a7fb436
Thomas Franquelin <thomas.franquelin@gmail.com>
2011-08-12 14:37:31 +0200

    First commit : standard directories plus user stories in the doc folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WizardRails2::Application.config.secret_token = 'aa88a21e7154f70a1fce0b0108447707fa39e517d514216ac004ee39adeef56d9188344e544ef75808f817060d3919f26dc73c43daa5627736f4681b59982b3c'
