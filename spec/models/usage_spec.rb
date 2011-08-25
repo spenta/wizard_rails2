aeefb0fe77f266e5df3e18a887d7410aaa3fd34c
Thomas Franquelin <thomas.franquelin@gmail.com>
2011-08-25 13:24:24 +0200

    complete previous commit

---
+++ spec/models/usage_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Usage do
+  before(:each) do
+    id = 1
+    %w{Bureautique Internet Mobilite}.each do |super_usage_name|
+      super_usage = Factory(:super_usage, :name => super_usage_name)
+      [1, 2].each do |usage_number|
+        Factory(:usage, :name => "#{super_usage_name}_#{usage_number}", :super_usage_id => super_usage.id, :id => id)
+        id += 1
+      end
+    end
+  end
+
+  describe 'all_except_mobilities' do
+    it 'returns all the usages which are not mobility-related' do
+      expected_usages_names = %w{Bureautique_1 Bureautique_2 Internet_1 Internet_2}
+      actual_usages_names = Usage.all_except_mobilities.collect{|u| u.name}
+      actual_usages_names.should eq(expected_usages_names)
+    end
+  end
+
+  describe 'all_except_mobilities_ids' do
+    it 'returns all the ids of the usages which are not mobility-related' do
+      expected_ids = [1, 2, 3, 4]
+      actual_ids = Usage.all_except_mobilities_ids
+      actual_ids.should eq(expected_ids)
+    end
+  end
+end
